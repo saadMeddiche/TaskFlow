87495e68043beca86fba83f023e5f3b32f833c07
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-20 10:26:52 +0100

    Create Dtos for signin , signUp and JwtTocken

---
+++ src/main/java/com/taskflow/taskmanagement/dtos/authentication/response/JwtAuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.taskflow.taskmanagement.dtos.authentication.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtAuthenticationResponse {
+    private String token;
+}
