ce8242675ddbb9a463f3fc1ea5d45a433dfd6880
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-20 09:20:58 +0100

    Create UserService interface and implementation

---
+++ src/main/java/com/taskflow/taskmanagement/services/UserService.java
@@ -0,0 +1,7 @@
+package com.taskflow.taskmanagement.services;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService {
+    UserDetailsService userDetailsService();
+}
---
+++ src/main/java/com/taskflow/taskmanagement/services/implementations/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package com.taskflow.taskmanagement.services.implementations;
+
+import com.taskflow.taskmanagement.repositories.UserRepository;
+import com.taskflow.taskmanagement.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetailsService userDetailsService(){
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            }
+        };
+    }
+}
