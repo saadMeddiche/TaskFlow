7cb9ebd9195b54ca92ea873196ef76e89334ee10
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-21 15:30:36 +0100

    Changes in default data

--- src/main/java/com/taskflow/taskmanagement/converters/TagConverter.java
+++ src/main/java/com/taskflow/taskmanagement/converters/TagConverter.java
@@ -3,12 +3,15 @@ package com.taskflow.taskmanagement.converters;
 import com.taskflow.taskmanagement.dtos.tag.request.TagRequest;
 import com.taskflow.taskmanagement.entities.Tag;
 
+import java.util.List;
+
 public class TagConverter {
 
     public static Tag convertToEntity(TagRequest tagRequest) {
         return Tag.builder()
                 .id(null)
                 .name(tagRequest.getName())
+                .tasks(List.of())
                 .build();
     }
 }
--- src/main/java/com/taskflow/taskmanagement/repositories/BaseRepository.java
+++ src/main/java/com/taskflow/taskmanagement/repositories/BaseRepository.java
@@ -2,10 +2,6 @@ package com.taskflow.taskmanagement.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-
-import java.io.Serializable;
 
 @NoRepositoryBean
 public interface BaseRepository<O> extends JpaRepository<O, Long> {
