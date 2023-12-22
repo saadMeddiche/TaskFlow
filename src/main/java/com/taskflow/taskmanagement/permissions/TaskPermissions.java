8ac16ebc048a247aa8fec555ed466fd8c121656f
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-22 14:55:17 +0100

    Create Permissions for Task

---
+++ src/main/java/com/taskflow/taskmanagement/permissions/TaskPermissions.java
@@ -0,0 +1,11 @@
+package com.taskflow.taskmanagement.permissions;
+
+public enum TaskPermissions {
+
+    CREATE_TASK,
+    ASSIGN_TASK,
+    MARK_TASK_AS_DONE,
+    REPLACE_TASK,
+    DEMAND_REPLACEMENT,
+    DELETE_TASK
+}
