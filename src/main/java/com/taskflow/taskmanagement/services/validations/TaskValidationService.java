f9417fe7e0e817666a1cc82a859b410b2de2f3d8
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-25 09:28:28 +0100

    Add Validation Of Dates when creating a task

--- src/main/java/com/taskflow/taskmanagement/services/validations/TaskValidationService.java
+++ src/main/java/com/taskflow/taskmanagement/services/validations/TaskValidationService.java
@@ -1,6 +1,7 @@
 package com.taskflow.taskmanagement.services.validations;
 
 import com.taskflow.taskmanagement.entities.Task;
+import com.taskflow.taskmanagement.handlingExceptions.costumExceptions.DateValidationException;
 import com.taskflow.taskmanagement.handlingExceptions.costumExceptions.ValidationException;
 import org.springframework.stereotype.Component;
 
@@ -11,11 +12,15 @@ public class TaskValidationService extends BaseValidation {
 
     private static final Predicate<Task> TASK_HAS_LESS_THAN_THREE_TAGS = task -> task.getTags().size() < 3;
 
+    private static final Predicate<Task> DATE_STARTS_AFTER_ENDS = task -> task.getStartDate().isAfter(task.getEndDate());
+
     public void validateTaskOnCreating(Task task) {
 
         validateObject(task);
 
         throwExceptionIf(TASK_HAS_LESS_THAN_THREE_TAGS, task, ValidationException::new, "Task must have at least 3 tags");
+
+        throwExceptionIf(DATE_STARTS_AFTER_ENDS, task, DateValidationException::new, "Start date must be before end date");
 
     }
 
