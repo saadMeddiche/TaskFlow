71a371a0f9709b67d07f74cfe328853b0bf17301
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-25 18:02:18 +0100

    TagValidationService use constructure

--- src/main/java/com/taskflow/taskmanagement/services/validations/TagValidationService.java
+++ src/main/java/com/taskflow/taskmanagement/services/validations/TagValidationService.java
@@ -4,24 +4,24 @@ import com.taskflow.taskmanagement.entities.Tag;
 import com.taskflow.taskmanagement.handlingExceptions.costumExceptions.AlreadyExistsException;
 import com.taskflow.taskmanagement.repositories.TagRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Predicate;
 
 @Component
-@RequiredArgsConstructor
 public class TagValidationService extends BaseValidation {
-
     private TagRepository tagRepository;
+
+    public TagValidationService(TagRepository tagRepository) {
+        this.tagRepository = tagRepository;
+    }
 
     private final Predicate<Tag> TAG_NAME_ALREADY_EXISTS = tag -> tagRepository.existsByName(tag.getName());
 
     public void validateTagOnCreating(Tag tag) {
-
         validateObject(tag);
-
         throwExceptionIf(TAG_NAME_ALREADY_EXISTS, tag, AlreadyExistsException::new, "Tag Name Already Exists");
     }
-
-
 }
+
