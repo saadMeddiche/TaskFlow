614f963470764e757be154b26559399b9c06752b
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-27 08:54:55 +0100

    Create Demand Service

---
+++ src/main/java/com/taskflow/taskmanagement/services/implementations/DemandServiceImpl.java
@@ -0,0 +1,11 @@
+package com.taskflow.taskmanagement.services.implementations;
+
+import com.taskflow.taskmanagement.entities.DemandReplacement;
+import com.taskflow.taskmanagement.services.DemandService;
+
+public class DemandServiceImpl implements DemandService {
+    @Override
+    public DemandReplacement createDemand(DemandReplacement demand) {
+        return null;
+    }
+}
