2c7a8ff1e05f9d9b8c6d21dcee910fdaf4984512
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-27 11:28:45 +0100

    Check if the task is replaced before  when creating a new demand

--- src/main/java/com/taskflow/taskmanagement/services/validations/DemandValidationService.java
+++ src/main/java/com/taskflow/taskmanagement/services/validations/DemandValidationService.java
@@ -1,11 +1,11 @@
 package com.taskflow.taskmanagement.services.validations;
 
 import com.taskflow.taskmanagement.entities.DemandReplacement;
+import com.taskflow.taskmanagement.entities.Task;
 import com.taskflow.taskmanagement.handlingExceptions.costumExceptions.ValidationException;
 import com.taskflow.taskmanagement.services.AuthenticationService;
 import com.taskflow.taskmanagement.services.CardService;
 import com.taskflow.taskmanagement.services.DemandService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Predicate;
@@ -19,23 +19,29 @@ public class DemandValidationService extends BaseValidation {
 
     private DemandService demandService;
 
-    public DemandValidationService(CardService cardService , AuthenticationService auth) {
+    public DemandValidationService(CardService cardService , AuthenticationService auth , DemandService demandService) {
         this.cardService = cardService;
         this.auth = auth;
+        this.demandService = demandService;
     }
 
     public void validateDemandOnCreating(DemandReplacement demand) {
 
         throwExceptionIf(CURRENT_TASK_IS_SAME_AS_NEW_TASK, demand , ValidationException::new , "The Current Task Is The Same As The New Task");
 
-        throwExceptionIf(AUTHENTICATE_USER_HAVE_ENOUGH_PERMISSIONS, null , ValidationException::new , "You Don't Have Enough Modification Cards");
+        throwExceptionIf(TASK_IS_REPLACED, demand.getCurrentTask() , ValidationException::new , "The Current Task Is Already Replaced");
 
+        throwExceptionIf(TASK_IS_REPLACED, demand.getNewTask() , ValidationException::new , "The New Task Is Already Replaced");
+
+        throwExceptionIf(AUTHENTICATE_USER_HAVE_ENOUGH_PERMISSIONS, null , ValidationException::new , "You Don't Have Enough Modification Cards");
 
     }
 
     private static final Predicate<DemandReplacement> CURRENT_TASK_IS_SAME_AS_NEW_TASK = demand -> demand.getCurrentTask().getId().equals(demand.getNewTask().getId());
 
     private final Predicate<Void> AUTHENTICATE_USER_HAVE_ENOUGH_PERMISSIONS = (Void) -> cardService.userCanUseModifyCard(auth.getCurrentAuthenticatedUser());
+
+    private final Predicate<Task> TASK_IS_REPLACED = (task) -> demandService.isTaskAReplaced(task.getId());
 
 
 }
