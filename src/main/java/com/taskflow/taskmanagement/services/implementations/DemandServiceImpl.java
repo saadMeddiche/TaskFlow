a3231ce8aeaa5db4034367b3cac0a50e38a660a5
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-28 09:00:47 +0100

    Remove One Card of modification when the demand is created with success

--- src/main/java/com/taskflow/taskmanagement/services/implementations/DemandServiceImpl.java
+++ src/main/java/com/taskflow/taskmanagement/services/implementations/DemandServiceImpl.java
@@ -2,6 +2,7 @@ package com.taskflow.taskmanagement.services.implementations;
 
 import com.taskflow.taskmanagement.entities.DemandReplacement;
 import com.taskflow.taskmanagement.repositories.DemandRepository;
+import com.taskflow.taskmanagement.services.CardService;
 import com.taskflow.taskmanagement.services.DemandService;
 import com.taskflow.taskmanagement.services.validations.DemandValidationService;
 import lombok.RequiredArgsConstructor;
@@ -14,9 +15,12 @@ public class DemandServiceImpl implements DemandService {
     private final DemandRepository demandRepository;
 
     private final DemandValidationService validation;
+
+    private final CardService cardService;
     @Override
     public DemandReplacement createDemand(DemandReplacement demand) {
         validation.validateDemandOnCreating(demand);
+        cardService.useModifyCard(demand.getDemandedBy());
         return demandRepository.save(demand);
     }
 
